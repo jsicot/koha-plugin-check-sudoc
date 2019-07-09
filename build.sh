ef4c83bab5e0c27b36bece3b6432c853a0751a59
jsicot <julien.sicot@gmail.com>
2019-07-09 10:37:25 +0200

    build ignore .DS_Store

--- build.sh
+++ build.sh
@@ -12,6 +12,6 @@ cp -r Koha dist/. ;
 perl -pi -e "s/{VERSION}/$VERSION/g" $FILEPATHFULLDIST ;
 perl -pi -e "s/{UPDATE_DATE}/$TODAY/g" $FILEPATHFULLDIST ;
 cd dist ;
-zip -r ../kpz/$KPZFILENAME ./Koha ;
+zip -r ../kpz/$KPZFILENAME ./Koha -x "*.DS_Store";
 cd .. ;
 rm -rf dist ;
